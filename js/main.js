f1fb3589cd908b44ad67b9fa98c58dd6cc7b3521
Nawal <lulay2020@gmail.com>
2020-04-22 19:26:44 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// scrolling effect
+ScrollOut({
+	threshold: .3,
+	once: true
+});
+
+// toggle dark mode
+
+let checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function(){
+	if (this.checked) {
+		trans()
+		document.documentElement.setAttribute('data-theme', 'dark')
+	}else{
+		trans()
+		document.documentElement.setAttribute('data-theme', 'light')
+	}
+})
+
+let trans = ()=>{
+	document.documentElement.classList.add('transition');
+	window.setTimeout(()=>{
+		document.documentElement.classList.remove('transition')
+	}, 1000)
+}
